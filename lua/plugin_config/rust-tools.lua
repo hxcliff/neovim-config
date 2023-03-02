ea10a6628ad6f603ec576c0aa16c4880316cc652
morn-0 <90960002+morn-0@users.noreply.github.com>
2023-03-02 11:22:35 +0000

    修改快捷键

--- lua/plugin_config/rust-tools.lua
+++ lua/plugin_config/rust-tools.lua
@@ -6,6 +6,13 @@ rt.setup({
       local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
       require('keybindings').mapLSP(buf_set_keymap)
-    end
+    end,
+    settings = {
+      ['rust-analyzer'] = {
+        cargo = {
+          -- target = "wasm32-unknown-unknown"
+        }
+      }
+    }
   }
 })
