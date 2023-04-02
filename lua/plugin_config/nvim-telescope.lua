780d9f7a6f992fbb6db28568d003671dfda224bf
morn-0 <90960002+morn-0@users.noreply.github.com>
2023-04-02 12:46:28 +0000

    update

--- lua/plugin_config/nvim-telescope.lua
+++ lua/plugin_config/nvim-telescope.lua
@@ -1,7 +1,15 @@
 local telescope = require('telescope')
 
 telescope.setup({
+  pickers = {
+    find_files = {
+      find_command = { "fd", "--hidden", "--no-require-git" }
+    },
+    live_grep = {
+      additional_args = function() return { "--hidden", "--no-require-git" } end
+    }
+  },
   defaults = {
-    borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└'},
+    borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└'}
   }
 })
