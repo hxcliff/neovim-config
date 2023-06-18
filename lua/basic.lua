b9f4c35f3299c3341daeabb51f81bae6ceccddfb
morn-0 <90960002+morn-0@users.noreply.github.com>
2023-06-18 15:33:44 +0000

    update

--- lua/basic.lua
+++ lua/basic.lua
@@ -71,7 +71,7 @@ vim.g.completeopt = 'menu,menuone,noselect,noinsert'
 vim.o.termguicolors = true
 -- 不可见字符的显示，这里只把空格显示为一个点
 vim.o.list = true
-vim.o.listchars = 'space:·,eol:↴'
+-- vim.o.listchars = 'space:·,eol:↴'
 -- 补全增强
 vim.o.wildmenu = true
 -- Dont' pass messages to |ins-completin menu|
