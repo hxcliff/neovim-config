1952038f9888ae930fe1d28424a407b2468386a3
morn-0 <90960002+morn-0@users.noreply.github.com>
2023-08-20 15:03:41 +0000

    update

--- lua/plugin_config/nvim-lspconfig.lua
+++ lua/plugin_config/nvim-lspconfig.lua
@@ -1,4 +1,5 @@
 local lspconfig = require('lspconfig')
+local lspconfig_utils = require("lspconfig.util")
 
 local on_attach = function(_, bufnr)
   local opt = { noremap = true, silent = true }
@@ -33,8 +34,63 @@ lspconfig.yamlls.setup({
   on_attach = on_attach,
 })
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.experimental = {
+  hoverActions = true,
+  hoverRange = true,
+  serverStatusNotification = true,
+  snippetTextEdit = true,
+  codeActionGroup = true,
+  ssr = true,
+}
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = { "documentation", "detail", "additionalTextEdits" },
+}
+capabilities.experimental.commands = {
+  commands = {
+    "rust-analyzer.runSingle",
+    "rust-analyzer.debugSingle",
+    "rust-analyzer.showReferences",
+    "rust-analyzer.gotoLocation",
+    "editor.action.triggerParameterHints",
+  },
+}
+
+local function get_root_dir(filename)
+  local fname = filename or vim.api.nvim_buf_get_name(0)
+  local cargo_crate_dir = lspconfig_utils.root_pattern("Cargo.toml")(fname)
+  local cmd = { "cargo", "metadata", "--no-deps", "--format-version", "1" }
+  if cargo_crate_dir ~= nil then
+    cmd[#cmd + 1] = "--manifest-path"
+    cmd[#cmd + 1] = lspconfig_utils.path.join(cargo_crate_dir, "Cargo.toml")
+  end
+  local cargo_metadata = ""
+  local cm = vim.fn.jobstart(cmd, {
+    on_stdout = function(_, d, _)
+      cargo_metadata = table.concat(d, "\n")
+    end,
+    stdout_buffered = true,
+  })
+  if cm > 0 then
+    cm = vim.fn.jobwait({ cm })[1]
+  else
+    cm = -1
+  end
+  local cargo_workspace_dir = nil
+  if cm == 0 then
+    cargo_workspace_dir = vim.fn.json_decode(cargo_metadata)["workspace_root"]
+  end
+  return cargo_workspace_dir
+      or cargo_crate_dir
+      or lspconfig_utils.root_pattern("rust-project.json")(fname)
+      or lspconfig_utils.find_git_ancestor(fname)
+end
+
 lspconfig.rust_analyzer.setup({
   on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = get_root_dir
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
