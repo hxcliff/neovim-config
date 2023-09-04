411b35aa8c62dd1ac3c9cc859b458238d6b0ad47
morn-0 <90960002+morn-0@users.noreply.github.com>
2023-09-04 16:22:16 +0000

    update

--- lua/plugin_config/blankline.lua
+++ lua/plugin_config/blankline.lua
@@ -17,8 +17,7 @@ local list = {
 }
 
 indent_blankline.setup({
-  -- space_char_blankline = ' ',
   show_current_context = true,
   show_current_context_start = true,
-  -- char_highlight_list = list
+  char_highlight_list = list
 })
