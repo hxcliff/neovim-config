3c635d6318c3e087a9d3088440d1bc5504a0d957
morn-0 <90960002+morn-0@users.noreply.github.com>
2023-09-05 05:43:57 +0000

    update

--- lua/plugin_config/cmp.lua
+++ lua/plugin_config/cmp.lua
@@ -1,12 +1,41 @@
 local lspkind = require('lspkind')
 local luasnip = require("luasnip")
 local cmp = require('cmp')
-
 require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup {
+  completion = {
+    completeopt = 'menu,menuone,noinsert',
+  },
   snippet = {
     expand = function(args) luasnip.lsp_expand(args.body) end
+  },
+  matching = {
+    disallow_fuzzy_matching = false,
+    disallow_fullfuzzy_matching = false,
+    disallow_partial_fuzzy_matching = false,
+  },
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.recently_used,
+      function(entry1, entry2)
+        local _, entry1_under = entry1.completion_item.label:find("^_+")
+        local _, entry2_under = entry2.completion_item.label:find("^_+")
+        entry1_under = entry1_under or 0
+        entry2_under = entry2_under or 0
+
+        if entry1_under > entry2_under then
+          return false
+        elseif entry1_under < entry2_under then
+          return true
+        end
+      end,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+    },
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
@@ -22,15 +51,15 @@ cmp.setup {
       mode = 'symbol',
       with_text = true,
       maxwidth = 50,
-      before = function(entry, vim_item)
-        vim_item.menu = '[' .. string.upper(entry.source.name) .. ']'
-        return vim_item
+      before = function(entry, item)
+        item.menu = '[' .. string.upper(entry.source.name) .. ']'
+        return item
       end
     })
   }
 }
 cmp.setup.cmdline({ '/', '?' }, {
-  completion = { autocomplete = false },
+  completion = { autocomplete = false, completeopt = "menu,menuone,noinsert,preview" },
   sources = {
     {
       name = 'buffer',
@@ -42,7 +71,7 @@ cmp.setup.cmdline({ '/', '?' }, {
 })
 
 cmp.setup.cmdline(':', {
-  completion = { autocomplete = false },
+  completion = { autocomplete = false, completeopt = "menu,menuone,noinsert,preview" },
   sources = cmp.config.sources({
     { name = 'path' }
   }, {
