5225274671d39ac57044cfcbb7df138581dfb652
morn-0 <90960002+morn-0@users.noreply.github.com>
2023-09-11 17:10:19 +0000

    update

--- lua/plugin_config/noice.lua
+++ lua/plugin_config/noice.lua
@@ -17,7 +17,7 @@ require('noice').setup({
     },
     hover = {
       border = {
-        style = 'single'
+        style = 'none'
       }
     },
     cmdline = {
@@ -44,6 +44,11 @@ require('noice').setup({
   lsp = {
     progress = {
       enabled = false
+    },
+    override = {
+      ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+      ['vim.lsp.util.stylize_markdown'] = true,
+      ['cmp.entry.get_documentation'] = true,
     }
   },
   presets = {
@@ -51,6 +56,6 @@ require('noice').setup({
     command_palette = true,
     long_message_to_split = true,
     inc_rename = false,
-    lsp_doc_border = true
+    lsp_doc_border = false
   }
 })
