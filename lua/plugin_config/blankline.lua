41f76234f4bc7dd382093691ed03437cc8dd0998
morn-0 <90960002+morn-0@users.noreply.github.com>
2023-10-05 09:39:45 +0000

    update

--- lua/plugin_config/blankline.lua
+++ lua/plugin_config/blankline.lua
@@ -1,23 +1,3 @@
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+local ibl = require('ibl')
 
-local indent_blankline = require('ibl')
-
-local list = {
-  'IndentBlanklineIndent1',
-  'IndentBlanklineIndent2',
-  'IndentBlanklineIndent3',
-  'IndentBlanklineIndent4',
-  'IndentBlanklineIndent5',
-  'IndentBlanklineIndent6'
-}
-
-indent_blankline.setup({
-  scope = {
-    enabled = true
-  }
-})
+ibl.setup({})
