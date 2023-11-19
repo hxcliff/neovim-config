63660117c98e5b0e4ce020cf75e8cdc512f81756
morn-0 <90960002+morn-0@users.noreply.github.com>
2023-11-19 10:58:52 +0000

    update

--- lua/plugin_config/toggleterm.lua
+++ lua/plugin_config/toggleterm.lua
@@ -5,7 +5,7 @@ toggleterm.setup({
     if term.direction == "horizontal" then
       return 15
     elseif term.direction == "vertical" then
-      return vim.o.columns * 0.4
+      return vim.o.columns * 0.35
     end
   end,
 })
