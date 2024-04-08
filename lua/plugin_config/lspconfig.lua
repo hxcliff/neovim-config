44148c8770142f56fdc3a89dc6bfbc298ba89f17
morn-0 <90960002+morn-0@users.noreply.github.com>
2024-04-08 08:57:29 +0000

    update

--- lua/plugin_config/lspconfig.lua
+++ lua/plugin_config/lspconfig.lua
@@ -260,39 +260,110 @@ lspconfig.dartls.setup({
   capabilities = dart_capabilities()
 })
 
-local function rust_root_dir(filename)
-  local fname = filename or vim.api.nvim_buf_get_name(0)
-  local cargo_crate_dir = lspconfig_util.root_pattern('Cargo.toml')(fname)
-  local cmd = { 'cargo', 'metadata', '--no-deps', '--format-version', '1' }
-  if cargo_crate_dir ~= nil then
-    cmd[#cmd + 1] = '--manifest-path'
-    cmd[#cmd + 1] = lspconfig_util.path.join(cargo_crate_dir, 'Cargo.toml')
-  end
-  local cargo_metadata = ''
-  local cm = vim.fn.jobstart(cmd, {
-    on_stdout = function(_, d, _)
-      cargo_metadata = table.concat(d, '\n')
-    end,
-    stdout_buffered = true,
+local M = {}
+
+M.joinpath = vim.fs.joinpath or function(...)
+  return (table.concat({ ... }, '/'):gsub('//+', '/'))
+end
+
+M.uv = vim.uv or vim.loop
+
+M.is_windows = function()
+  local sysname = M.uv.os_uname().sysname
+  return sysname == 'Windows' or sysname == 'Windows_NT'
+end
+
+M.get_clients = vim.lsp.get_clients or vim.lsp.get_active_clients
+
+M.get_active_rustaceanvim_clients = function(bufnr, filter)
+  filter = vim.tbl_deep_extend('force', filter or {}, {
+    name = 'rust-analyzer',
   })
-  if cm > 0 then
-    cm = vim.fn.jobwait({ cm })[1]
-  else
-    cm = -1
+  if bufnr then
+    filter.bufnr = bufnr
   end
+  return M.get_clients(filter)
+end
+
+M.starts_with_windows_drive_letter = function(path)
+  return path:match('^%a:') ~= nil
+end
+
+M.normalize_path_on_windows = function(path)
+  if M.is_windows() and M.starts_with_windows_drive_letter(path) then
+    return path:sub(1, 1):lower() .. path:sub(2):gsub('/+', '\\')
+  end
+  return path
+end
+
+local function get_active_client_root(file_name)
+  local cargo_home = M.uv.os_getenv('CARGO_HOME') or M.joinpath(vim.env.HOME, '.cargo')
+  local registry = M.joinpath(cargo_home, 'registry', 'src')
+
+  local rustup_home = M.uv.os_getenv('RUSTUP_HOME') or M.joinpath(vim.env.HOME, '.rustup')
+  local toolchains = M.joinpath(rustup_home, 'toolchains')
+
+  for _, item in ipairs { toolchains, registry } do
+    item = M.normalize_path_on_windows(item)
+    if file_name:sub(1, #item) == item then
+      local clients = M.get_active_rustaceanvim_clients()
+      return clients and #clients > 0 and clients[#clients].config.root_dir or nil
+    end
+  end
+end
+
+local function rust_root_dir(file_name)
+  local reuse_active = get_active_client_root(file_name)
+  if reuse_active then
+    return reuse_active
+  end
+  local path = vim.fs.dirname(file_name)
+  if not path then
+    return nil
+  end
+  local cargo_crate_dir = vim.fs.dirname(vim.fs.find({ 'Cargo.toml' }, {
+    upward = true,
+    path = path,
+  })[1])
   local cargo_workspace_dir = nil
-  if cm == 0 then
-    cargo_workspace_dir = vim.fn.json_decode(cargo_metadata)['workspace_root']
+  if vim.fn.executable('cargo') == 1 then
+    local cmd = { 'cargo', 'metadata', '--no-deps', '--format-version', '1' }
+    if cargo_crate_dir ~= nil then
+      cmd[#cmd + 1] = '--manifest-path'
+      cmd[#cmd + 1] = M.joinpath(cargo_crate_dir, 'Cargo.toml')
+    end
+    local cargo_metadata = ''
+    local cm = vim.fn.jobstart(cmd, {
+      on_stdout = function(_, d, _)
+        cargo_metadata = table.concat(d, '\n')
+      end,
+      stdout_buffered = true,
+      cwd = path,
+    })
+    if cm > 0 then
+      cm = vim.fn.jobwait({ cm })[1]
+    else
+      cm = -1
+    end
+    if cm == 0 then
+      cargo_workspace_dir = vim.fn.json_decode(cargo_metadata)['workspace_root']
+    end
   end
   return cargo_workspace_dir
       or cargo_crate_dir
-      or lspconfig_util.root_pattern('rust-project.json')(fname)
-      or lspconfig_util.find_git_ancestor(fname)
+      or vim.fs.dirname(vim.fs.find({ 'rust-project.json' }, {
+        upward = true,
+        path = path,
+      })[1])
 end
 
 local function rust_capabilities()
   local capabilities = protocol.make_client_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
+  capabilities.textDocument.foldingRange = {
+    dynamicRegistration = false,
+    lineFoldingOnly = true,
+  }
   capabilities.experimental = {
     hoverActions = true,
     hoverRange = true,
@@ -316,6 +387,8 @@ local function rust_capabilities()
 end
 
 lspconfig.rust_analyzer.setup({
+  name = 'rust-analyzer',
+  filetypes = { 'rust' },
   on_attach = on_attach,
   root_dir = rust_root_dir,
   capabilities = rust_capabilities(),
@@ -329,12 +402,15 @@ lspconfig.rust_analyzer.setup({
       },
       inlayHints = {
         maxLength = 512
+      },
+      checkOnSave = {
+        allFeatures = true,
+        command = 'clippy',
+        extraArgs = { '--no-deps' },
       }
     }
   }
 })
-
-local group = vim.api.nvim_create_augroup('RustToolsAutocmds', { clear = true })
 
 vim.api.nvim_create_autocmd('BufWritePost', {
   pattern = '*/Cargo.toml',
@@ -351,5 +427,5 @@ vim.api.nvim_create_autocmd('BufWritePost', {
       end
     end
   end,
-  group = group,
+  group = vim.api.nvim_create_augroup('RustToolsAutocmds', { clear = true }),
 })
