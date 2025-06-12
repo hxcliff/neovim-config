899fa7c44ebf1945bec336084b1779e09851fd83
morn-0 <90960002+morn-0@users.noreply.github.com>
2025-06-12 13:50:49 +0800

    update

--- lua/plugin_config/mason.lua
+++ lua/plugin_config/mason.lua
@@ -2,6 +2,8 @@ local mason = require('mason')
 local mason_lspconfig = require('mason-lspconfig')
 
 mason.setup({})
+
 mason_lspconfig.setup({
+  automatic_enable = false,
   ensure_installed = { 'lua_ls', 'jsonls', 'taplo', 'yamlls', 'html', 'cssls', 'rust_analyzer' }
 })
