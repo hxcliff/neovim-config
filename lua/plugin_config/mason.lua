8e9fbc2f0fe457d3cd094d195a223124b87353b3
hxcliff <90960002+hxcliff@users.noreply.github.com>
2025-07-14 06:44:16 +0000

    update

--- lua/plugin_config/mason.lua
+++ lua/plugin_config/mason.lua
@@ -5,5 +5,5 @@ mason.setup({})
 
 mason_lspconfig.setup({
   automatic_enable = false,
-  ensure_installed = { 'lua_ls', 'jsonls', 'taplo', 'yamlls', 'html', 'cssls', 'rust_analyzer' }
+  ensure_installed = { 'lua_ls', 'jsonls', 'taplo', 'yamlls', 'html', 'cssls', 'ts_ls', 'clangd', 'rust_analyzer' }
 })
